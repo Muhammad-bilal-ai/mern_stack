53c5b53d29e0f994926766d91314640b68fbe115
Muhammad Bilal <withbilal777@gmail.com>
2025-06-23 10:33:49 +0500

    project setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/products", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+  console.log("server started at https://localhost:5000");
+});
