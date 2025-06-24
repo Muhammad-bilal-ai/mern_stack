45c905802a72838d1b90002f657391bb25bef6fe
Muhammad Bilal <withbilal777@gmail.com>
2025-06-24 07:25:31 +0500

    mongooseDB connection, product model, create new product api created and tested.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,12 @@
+import e from "express";
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONOGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected ${conn.connection.host}`);
-  } catch (error) {}
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); //process code 1 means exit with failure, 0 mean success.
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); // allow the app to use json data in req.body
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user sends this data to database to create product
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all fields" });
+  }
 
-console.log(process.env.MONGO_URI);
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in create product", error.message);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+});
 
 app.listen(5000, () => {
+  connectDB();
   console.log("server started at https://localhost:5000");
 });
